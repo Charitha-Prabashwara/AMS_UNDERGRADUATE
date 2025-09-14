9aa3a4cfc23ce901f2b3611e65fa810d20e0c55e
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:19:29 +0530

    CreateDepartmentHead.js

---
+++ backend/src/classes/USERS/DepartmentHead.js
@@ -0,0 +1,18 @@
+const User = require('./User');
+const {userTypes} = require('../../config')
+class DepartmentHead extends User {
+    constructor(data = {}) {
+        super({ ...data, type: userTypes.USER_DEPARTMENT });
+
+         Object.defineProperty(this, '_type', {
+            value: userTypes.USER_DEPARTMENT,
+            writable: false, 
+            enumerable: true,
+            configurable: false
+        });
+    }
+    
+
+}
+
+module.exports = DepartmentHead;
