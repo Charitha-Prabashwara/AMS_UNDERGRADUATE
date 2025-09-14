55562329e3ba4b3776a8658f652d5e0fdd5a9f42
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:53:18 +0530

    Update createUserDTO.js

--- backend/src/classes/DTO/userDTO/createUserDTO.js
+++ backend/src/classes/DTO/userDTO/createUserDTO.js
@@ -1,24 +1,37 @@
 const Joi = require("joi");
 const {ValidationFailedError} = require('../../../errors')
+const {userTypes} = require('../../../config');
 class CreateUserDTO {
   constructor(data) {
     const schema = Joi.object({
             
         registrationId: Joi.string().max(20).required(),
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        fullName: Joi.string().required(),
-        nameWithInitial: Joi.string().required(),
+        firstName: Joi.string().pattern(/^[A-Z. ]+$/).required()
+                              .messages({ "string.pattern.base": "First name must be uppercase letters only" }),
+
+        lastName: Joi.string().pattern(/^[A-Z. ]+$/).required()
+                              .messages({ "string.pattern.base": "Last name must be uppercase letters only" }),
+
+        fullName: Joi.string().pattern(/^[A-Z. ]+$/).required()
+                              .messages({ "string.pattern.base": "Full name must be uppercase letters only" }),
+        
+        nameWithInitial: Joi.string().pattern(/^[A-Z. ]+$/).required()
+                              .messages({ "string.pattern.base": "Full name must be uppercase letters only" }),
         
         email: Joi.string().email().required(),
         addressLine1: Joi.string().required(),
         addressLine2: Joi.string(),
-        addressZip: Joi.string().required(),
+        addressZip: Joi.number().integer().required(),
 
-        type: Joi.string().valid('user', 'admin', 'student', 'department', 'lecturer').required(),
+        type: Joi.string().valid(...userTypes.USER_TYPES).required(),
 
         password: Joi.string().min(6).required(),
-        confirmPassword: Joi.string().min(6).required(),
+        confirmPassword:Joi.string()
+                        .valid(Joi.ref('password'))
+                        .required()
+                        .messages({
+                          'any.only': 'Confirm password must match password'
+                        }),
         
     });
 
