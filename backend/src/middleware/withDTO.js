62471850b9885e695a2dcf285e0161bd815ff3b3
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:58:53 +0530

    Update withDTO

--- backend/src/middleware/withDTO.js
+++ backend/src/middleware/withDTO.js
@@ -1,6 +1,6 @@
 function withDTO(DTOClass, controllerFn) {
   return async (req, res, next) => {
-      await controllerFn(new DTOClass(req.body), req, res);
+      await controllerFn(new DTOClass(req.body), req, res, next);
   };
 }
 
