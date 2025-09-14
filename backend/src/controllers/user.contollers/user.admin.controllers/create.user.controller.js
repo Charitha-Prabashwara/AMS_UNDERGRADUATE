c7fb0969a61d2c8c2b1979033af7b503021609c7
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:59:25 +0530

    Update create.user.controller

--- backend/src/controllers/user.contollers/user.admin.controllers/create.user.controller.js
+++ backend/src/controllers/user.contollers/user.admin.controllers/create.user.controller.js
@@ -1,12 +1,23 @@
 const  {UserBuilder, AdminBuilder, User, Admin} = require('../../../classes/USERS')
+const {UserAccountService} = require('../../../services')
+const {DuplicateKeyError, ValidationError} = require('../../../errors/')
 
-exports.createUser = async(dto,req, res)=>{
-    return res.status(200).json(
+exports.createUser = async(dto,req, res, next)=>{
+    try {
+        const service  = new UserAccountService()
+        const user = await service.createUser(dto);
+        
+
+         return res.status(200).json(
         {
             success:true,
             message: "api/v1 is working...",
-            dto:dto
-        },
+            dto:user
+        })
         
-    )
+    
+    } catch (error) {
+       next(error)
+    }
+   
 }
