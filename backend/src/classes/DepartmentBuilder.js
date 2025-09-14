b054e15536fda23d2ee3930979367f159cfca5b9
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:20:34 +0530

    Create DepartmentBuilder.js

---
+++ backend/src/classes/DepartmentBuilder.js
@@ -0,0 +1,45 @@
+const Department = require('./Department')
+const {DepartmentRepository} = require('./DATABASE')
+class DepartmentBuilder{
+    id
+    name
+    description
+    deleted
+    createdAt_timestamp
+    updatedAt_timestamp
+
+    constructor(data={}){
+        this.id = data._id
+        this.name = data.name
+        this.description = data.description
+        this.deleted = data.deleted;
+        this.createdAt_timestamp = data.createdAt_timestamp
+        this.updatedAt_timestamp = data.updatedAt_timestamp
+        
+        this.repository = new DepartmentRepository();
+    }
+
+    async create(){
+         try {
+            const fields = [
+                'id', 'name' ,'description', 'deleted', 'createdAt_timestamp', 'updatedAt_timestamp'
+            ]
+            const params = {}
+
+            for(const field of fields){
+                if(this[field] !== undefined){
+                    params[field] = this[field]
+                }
+            }
+
+            const dept = await this.repository.create(params)
+            return new Department(dept)
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+}
+
+module.exports = DepartmentBuilder;
