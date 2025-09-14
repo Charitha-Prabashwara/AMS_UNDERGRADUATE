e96eb81a869c4bf9cf61f4fce319e2f2bdaaaf86
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:57:04 +0530

    Update error index

--- backend/src/errors/index.js
+++ backend/src/errors/index.js
@@ -1,5 +1,18 @@
 const UserNotFoundError = require('./RepositoryErrors/UserRepositoryErrors/UserNotFoundError');
 const InvalidUserIdError = require('./RepositoryErrors/UserRepositoryErrors/InvalidUserIdError');
 const ValidationFailedError = require('./DataTransferObjectErrors/ValidationFailedError')
+const DuplicateKeyError = require('./RepositoryErrors/UserRepositoryErrors/DuplicateKeyError')
+const ValidationError = require('./RepositoryErrors/UserRepositoryErrors/ValidationError')
 
-module.exports = {UserNotFoundError, InvalidUserIdError, ValidationFailedError}
+const DepartmentNotFoundError = require('./RepositoryErrors/DepartmentRepositoryErrors/DepartmentNotFoundError')
+const InvalidDepartmentIdError = require('./RepositoryErrors/DepartmentRepositoryErrors/InvalidDepartmentIdError')
+
+module.exports = {
+    UserNotFoundError,
+    InvalidUserIdError,
+    ValidationFailedError,
+    DuplicateKeyError,
+    ValidationError,
+    DepartmentNotFoundError,
+    InvalidDepartmentIdError
+}
