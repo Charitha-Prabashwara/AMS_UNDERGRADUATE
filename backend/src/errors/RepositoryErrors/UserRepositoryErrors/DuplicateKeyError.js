c2004246231e289520cca966de4aedae2fd255d5
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-09-14 16:56:18 +0530

    Create Exceptions

---
+++ backend/src/errors/RepositoryErrors/UserRepositoryErrors/DuplicateKeyError.js
@@ -0,0 +1,16 @@
+const UserRepositoryError = require('./UserRepositoryError')
+
+class DuplicateKeyError extends UserRepositoryError{
+    constructor(error){
+       super("Duplicate Key Error: ", 409);
+        
+        this.details = {
+            keys: error?.keyValue 
+            ? Object.keys(error.keyValue) 
+            : []
+        }
+       
+    }
+}
+
+module.exports = DuplicateKeyError
