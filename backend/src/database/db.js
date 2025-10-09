398516476a35048c8a6fdc78b4ae9e5b0ed052a9
Charitha-Prabashwara <prabhashwara.seu@gmail.com>
2025-10-09 11:25:11 +0530

    Update db file

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -4,7 +4,6 @@ const {config} = require('../config');
 const DB_connect = async () => {
   try {
     await mongoose.connect(config.DB_MONGODB_URI + config.DB_MONGODB_DATABASE);
-    console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1); // Exit process on DB connection failure
