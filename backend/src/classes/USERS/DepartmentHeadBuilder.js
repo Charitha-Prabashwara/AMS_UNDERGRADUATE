d80365dc81c097b4d829b4c5766e903d488ee6a7
Charitha-Prabashwara <prabhashwara.seu@gmail.com>
2025-10-12 16:56:03 +0530

    my overthinking recovery.

--- backend/src/classes/USERS/DepartmentHeadBuilder.js
+++ backend/src/classes/USERS/DepartmentHeadBuilder.js
@@ -2,8 +2,7 @@ const UserBuilder = require('./UserBuilder');
 const {userTypes} = require('../../config');
 
 class DepartmentHeadBuilder extends UserBuilder {
-    /** @type {string} */
-    _department
+   
     
     constructor(data = {}) {
         super({...data, type: userTypes.USER_DEPARTMENT});
@@ -16,9 +15,7 @@ class DepartmentHeadBuilder extends UserBuilder {
         });
     }
 
-    get department(){return this._department}
-    set department(department){this._department = department}
-
+    
 }
 
 module.exports = DepartmentHeadBuilder;
