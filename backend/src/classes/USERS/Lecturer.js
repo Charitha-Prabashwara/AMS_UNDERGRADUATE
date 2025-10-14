e3e7c5a2a256167cf9a44fc8a187c4e0581674a3
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-10-14 21:04:22 +0530

    Update lecturer, lecturerBuilder and test

--- backend/src/classes/USERS/Lecturer.js
+++ backend/src/classes/USERS/Lecturer.js
@@ -4,14 +4,44 @@ class Lecturer extends User {
     constructor(data = {}) {
         super({ ...data, type: userTypes.USER_LECTURER });
 
-         Object.defineProperty(this, '_type', {
-            value: userTypes.USER_LECTURER,
-            writable: false, 
+        Object.defineProperty(this, '_type', {
+            get() { return userTypes.USER_LECTURER; },
+            set() { throw new TypeError('Cannot modify _type'); },
             enumerable: true,
             configurable: false
         });
     }
     
+    static _wrapToLecturer(obj) {
+        if (!obj) return obj;
+        if (obj instanceof Lecturer) return obj;
+        return new Lecturer(obj);
+    }
+
+    async save(){
+        const user = await super.save()
+        return Lecturer._wrapToLecturer(user);
+    }
+
+    async findById(id){
+        const user = await super.findById(id);
+        return  Lecturer._wrapToLecturer(user)
+    }
+
+    async find(){
+        const users = await super.find();
+        return users.map(user =>  Lecturer._wrapToLecturer(user));
+    }
+    async deleteOne(){
+        const user = await super.deleteOne()
+        return  Lecturer._wrapToLecturer(user)
+    }
+
+    async deleteById(id){
+        const user = await super.deleteById(id)
+        return  Lecturer._wrapToLecturer(user)
+    }
+
 
 }
 
--- backend/src/classes/USERS/LecturerBuilder.js
+++ backend/src/classes/USERS/LecturerBuilder.js
@@ -1,15 +1,20 @@
 const UserBuilder = require('./UserBuilder');
-const {userTypes} = require('../../config')
+const {userTypes} = require('../../config');
+const Lecturer = require('./Lecturer');
 class LecturerBuilder extends UserBuilder {
     constructor(data = {}) {
         super({...data, type: userTypes.USER_LECTURER});
         
         Object.defineProperty(this, '_type', {
-            value: userTypes.USER_LECTURER,
-            writable: false, 
+            get() { return userTypes.USER_LECTURER; },
+            set() { throw new TypeError('Cannot modify _type'); },
             enumerable: true,
             configurable: false
         });
+    }
+    async create(){
+        const user = await super.create()
+        return new Lecturer(user)
     }
 
 }
