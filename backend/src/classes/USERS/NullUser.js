e607d6f93bbd991ab070ace73d114e8dd5d8b37d
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-10-21 14:03:37 +0530

    create immutable null object using singleton pattern #7

--- backend/src/classes/USERS/NullUser.js
+++ backend/src/classes/USERS/NullUser.js
@@ -1,14 +1,9 @@
 class NullUser {
   constructor() {
-    if (NullUser.instance) {
-      return NullUser.instance;
-    }
-
+    if (NullUser.instance) {return NullUser.instance}
     Object.freeze(this);
     NullUser.instance = this;
   }
-
-
 }
 
 module.exports = new NullUser();
