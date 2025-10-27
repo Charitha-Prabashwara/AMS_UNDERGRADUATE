4840360fd4a021f7cca3f490031e9739813c0f72
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-10-27 06:15:11 +0530

    Create service can manage all user-account services

--- backend/src/services/UserAccountService.js
+++ backend/src/services/UserAccountService.js
@@ -1,57 +1,18 @@
-const {User, Admin} = require('../classes/USERS')
-const AdminBuilder = require('../classes/USERS/AdminBuilder');
-const DepartmentHeadBuilder = require('../classes/USERS/DepartmentHeadBuilder')
+const {User, Admin, NullUser} = require('../classes/USERS')
 const PasswordHashService = require('./PasswordHashService')
 const {userTypes} = require('../config')
+const {selectCorrectUser} = require('./dependencies/userServicesSupport')
+const {UserNotFoundError} = require('../errors')
+
 class UserAccountService{
 
-    async createUser(dto){
-        if(dto.type == userTypes.USER_ADMIN){
-            const admin = new AdminBuilder();
-
-            admin.registration_id = dto.registrationId;
-            admin.name = {
-                first_name: dto.firstName,
-                last_name: dto.lastName,
-                full_name: dto.fullName,
-                with_initial_name: dto.nameWithInitial
-            };
-            admin.email = dto.email;
-            admin.address = {
-                line1:dto.addressLine1,
-                line2:dto.addressLine2,
-                zip:dto.addressZip
-            };
-            admin.password = await PasswordHashService.hashPassword(dto.password);
-
-            const adminUser = await admin.create()
-            return adminUser;
-
-        }
-
-        if(dto.type == userTypes.USER_DEPARTMENT){
-            const departmentHead = new DepartmentHeadBuilder();
-
-            departmentHead.registration_id = dto.registrationId;
-            departmentHead.name = {
-                first_name: dto.firstName,
-                last_name: dto.lastName,
-                full_name: dto.fullName,
-                with_initial_name: dto.nameWithInitial
-            };
-            departmentHead.email = dto.email;
-            departmentHead.address = {
-                line1:dto.addressLine1,
-                line2:dto.addressLine2,
-                zip:dto.addressZip
-            };
-            departmentHead.password = await PasswordHashService.hashPassword(dto.password);
-
-            const departmentHeadUser = await departmentHead.create()
-            return departmentHeadUser;
-        }
-        
-    }
+   static async getUserById(userType, id){
+        const userClass = selectCorrectUser(userType)
+        const user = await userClass.findById(id);
+        if(user === NullUser) throw UserNotFoundError()
+        return user;
+   }
+  
 
 
 }
