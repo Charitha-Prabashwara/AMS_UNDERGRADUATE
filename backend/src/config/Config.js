8a4ce54f2ba7c2a6d860723780920eada9e26dc9
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-11-16 02:14:46 +0530

    config bugfix

--- backend/src/config/Config.js
+++ backend/src/config/Config.js
@@ -9,7 +9,7 @@ class Config{
             this.DB_MONGODB_DATABASE = env.DB_MONGODB_DATABASE;
             this.APPLICATION_PORT = env.APPLICATION_PORT;
             this.SLAT_ROUNDS = env.SLAT_ROUNDS;
-            this.ACCESS_TOKEN_SECRET = env.REFRESH_TOKEN_SECRET;
+            this.ACCESS_TOKEN_SECRET = env.ACCESS_TOKEN_SECRET;
             this.REFRESH_TOKEN_SECRET = env.REFRESH_TOKEN_SECRET;
             this.ACCESS_TOKEN_TTL = env.ACCESS_TOKEN_TTL;
             this.REFRESH_TOKEN_TTL = env.REFRESH_TOKEN_TTL;
