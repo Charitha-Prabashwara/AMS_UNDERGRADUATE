148cb78d63378ab6be004ade19a69bf2d0c8ade8
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-11-16 16:31:50 +0530

    create helper for auth service dependency inversion

---
+++ backend/src/services/authServiceHelper.js
@@ -0,0 +1,76 @@
+
+
+class AuthTokenServiceHelper{
+    constructor(authTokenService){
+        this.authTokenService = authTokenService
+    }
+
+    generatePayload(userType, id, expiresTimestamp, priority=0,other={}){
+        return this.authTokenService.generatePayload(userType, id, expiresTimestamp, priority=0,other={})
+    }
+
+    generateAccessToken(payload){
+        return this.authTokenService.generateAccessToken(payload)
+    }
+
+    generateRefreshToken(payload){
+        return this.authTokenService.generateRefreshToken(payload)
+    }
+
+    verifyAccessToken(token){
+        return this.authTokenService.verifyAccessToken(token)
+    }
+
+    verifyRefreshToken(token){
+        return this.authTokenService.verifyRefreshToken(token)
+    }
+}
+
+class PasswordHashServiceHelper{
+
+  constructor(passwordHashService){
+    this.passwordHashService = passwordHashService
+  }
+
+  async hashPassword(password){
+    return await this.passwordHashService.hashPassword(password)
+  }
+
+  async verifyPassword(password, hashedPassword){
+    return await this.passwordHashService.verifyPassword(password, hashedPassword);
+  }
+
+}
+
+class UserServiceHelper{
+
+  constructor(userAccountService) {
+    this.userAccountService = userAccountService
+  }
+
+  async getUserById(userType, id){
+    return await this.userAccountService.getUserById(userType, id)
+  }
+
+  async getUserByEmail(userType, email){
+    return await this.userAccountService.getUserByEmail(userType, email)
+  }
+
+  async findByIdAndUpdate(userType, user){
+    return await this.userAccountService.findByIdAndUpdate(userType, user)
+  }
+
+  isSuspended(user){
+    return this.userAccountService.isSuspended(user);
+  }
+
+  isNullUser(user){
+
+    return this.userAccountService.isNullUser(user)
+  }
+
+
+
+}
+
+module.exports = {AuthTokenServiceHelper, PasswordHashServiceHelper, UserServiceHelper}
