803ee287eb4edbd862aec114a580d55f29f9b6d4
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-04 01:04:51 +0530

    update DTOS

--- backend/src/classes/DTO/index.js
+++ backend/src/classes/DTO/index.js
@@ -2,4 +2,6 @@ const {CreateUserDTO} = require('./userDTO/createUserDTO');
 const {ErrorResponseDTO} =require('./errorResponseDTO')
 const {GetUserByIdDTO} = require('./userDTO/getUserByIdDTO')
 const {GetUserByRegistrationIdDTO} = require('./userDTO/getUserByRegistrationId')
-module.exports = {CreateUserDTO, ErrorResponseDTO, GetUserByIdDTO, GetUserByRegistrationIdDTO}
+const {getUserByEmailDTO} = require('./userDTO/getUserByEmail')
+const {UpdateUserByIdDTO} = require('./userDTO/updateUserByIdDTO')
+module.exports = {CreateUserDTO, ErrorResponseDTO, GetUserByIdDTO, GetUserByRegistrationIdDTO, getUserByEmailDTO, UpdateUserByIdDTO}
