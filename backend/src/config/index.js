d3f351bb78a8282cc4abdc3e5653230131942955
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-04 01:03:57 +0530

    update config index

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,4 +2,5 @@ const config = require('./Config')
 const userTypes = require('./UserTypes')
 const envTypes = require('./EnvTypes')
 const httpStatus = require('./HttpStatus')
-module.exports = {config, userTypes, envTypes, httpStatus};
+const Strings = require('./Strings')
+module.exports = {config, userTypes, envTypes, httpStatus, Strings};
