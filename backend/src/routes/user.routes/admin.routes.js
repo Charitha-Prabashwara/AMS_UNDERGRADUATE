14b4d8bb25c50ccbfa7cbebc855a0ede19b42528
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-04 01:05:49 +0530

    update admin router

--- backend/src/routes/user.routes/admin.routes.js
+++ backend/src/routes/user.routes/admin.routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { create } = require("../../controllers");
+const { create } = require("../../controllers/user.contollers/user.admin.controllers/create.user.controller");
+const { getUserById, getUserByRegistrationId, getUserByEmailId} = require("../../controllers/user.contollers/user.admin.controllers/get.user.controller");
+const {updateById} = require('../../controllers/user.contollers/user.admin.controllers/update.user.controller')
+const {userDeleteById} = require('../../controllers/user.contollers/user.admin.controllers/delete.user.controller')
 const {withDTO} = require("../../middleware");
-const { CreateUserDTO } = require("../../classes/DTO");
+const { CreateUserDTO, GetUserByIdDTO, GetUserByRegistrationIdDTO, getUserByEmailDTO, UpdateUserByIdDTO} = require("../../classes/DTO");
 
 const adminRouter = express.Router();
 adminRouter.post("/", withDTO(CreateUserDTO, create));
+adminRouter.get("/id/", withDTO(GetUserByIdDTO, getUserById));
+adminRouter.get("/reg/", withDTO(GetUserByRegistrationIdDTO, getUserByRegistrationId))
+adminRouter.get("/email/", withDTO(getUserByEmailDTO, getUserByEmailId))
+
+adminRouter.put('/id/', withDTO(UpdateUserByIdDTO, updateById))
+adminRouter.delete('/id/', withDTO(GetUserByIdDTO, userDeleteById))
 module.exports = adminRouter;
