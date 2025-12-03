3a92d84bff529a41f280326bc06ecc5c1f04176a
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-04 01:05:20 +0530

    update user controllers

--- backend/src/controllers/user.contollers/user.admin.controllers/get.user.controller.js
+++ backend/src/controllers/user.contollers/user.admin.controllers/get.user.controller.js
@@ -1,26 +1,66 @@
-const  {UserBuilder, AdminBuilder, User, Admin} = require('../../../classes/USERS')
-const {UserAccountService} = require('../../../services')
-const {DuplicateKeyError, ValidationError} = require('../../../errors/')
+const UserService = require('../../../services/UserService')
+const userService = new UserService()
+const {httpStatus, Strings} = require('../../../config')
+const {UserNotFoundError} = require('../../../errors')
 
-exports.getUserById = async(req, res, next)=>{
+exports.getUserById = async(dto,req, res, next)=>{
     try {
-       const userType = req.params.userType
-       const id = req.params.id
-
-     const user = await UserAccountService.getUserById(id, userType);
-
-       return res.status(200).json(
-        {
-            success:true,
-            message: "api/v1 is working...",
-            user:user
-
-           
-        })
+        const user = await userService.getUserById(dto.type, dto.id)
+        if(userService.isNullUser(user)) throw new UserNotFoundError()
         
-    
+        return res.status(httpStatus.OK).json(
+            {
+                success:true,
+                data:{
+                    user:user
+                }
+
+            }
+        )
     } catch (error) {
        next(error)
-    }
-   
+    } 
+}
+
+exports.getUserByRegistrationId = async(dto,req, res, next)=>{
+    try {
+        const user = await userService.getUserByRegistrationId(dto.type, dto.registrationId)
+        if(userService.isNullUser(user)) throw new UserNotFoundError()
+        
+        return res.status(httpStatus.OK).json(
+            {
+                success:true,
+                data:{
+                    user:user
+                }
+
+            }
+        )
+    } catch (error) {
+       next(error)
+    } 
+}
+
+exports.getUserByEmailId = async(dto,req, res, next)=>{
+    try {
+        const user = await userService.getUserByEmail(dto.type, dto.email)
+        if(userService.isNullUser(user)) throw new UserNotFoundError('ERROR_USER_NOT_FOUND', 'si')
+        
+        return res.status(httpStatus.OK).json(
+            {
+                success:true,
+                data:{
+                    user:user
+                }
+
+            }
+        )
+    } catch (error) {
+       next(error)
+    } 
+}
+
+
+exports.allUsers = async(dto, req, res, next)=>{
+    
 }
