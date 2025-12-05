0c8c65715679abd4fae15e9f80bd620a236005aa
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-05 13:14:46 +0530

    configure pm2

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "ams-api",
+      script: "src/server.js",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "200M",
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+        PORT: 8080,
+        WATCH: true  
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8080,
+        WATCH: false
+      },
+      watch: process.env.NODE_ENV === "development" 
+    }
+  ]
+};
