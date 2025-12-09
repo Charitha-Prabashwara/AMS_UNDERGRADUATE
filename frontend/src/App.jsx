53911d722910fee0a3498a82cca4f09cb8acea86
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-09 17:00:28 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
-
+import { SWRConfig } from 'swr';
+import { fetcher } from './api/fetcher';
 // project imports
 import router from 'routes';
 import ThemeCustomization from 'themes';
@@ -10,10 +11,21 @@ import ScrollTop from 'components/ScrollTop';
 
 export default function App() {
   return (
+        <SWRConfig
+      value={{
+        fetcher,
+        onError: (err) => {
+          console.error('SWR global error:', err);
+        },
+        revalidateOnFocus: true, // auto refresh on window focus
+        shouldRetryOnError: false
+      }}
+    >
     <ThemeCustomization>
       <ScrollTop>
         <RouterProvider router={router} />
       </ScrollTop>
     </ThemeCustomization>
+     </SWRConfig>
   );
 }
